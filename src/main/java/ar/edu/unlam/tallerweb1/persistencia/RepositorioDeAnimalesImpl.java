46213dc285ff367109f7c491c31364f8818d1967
Rocio Isaurralde <rocio.isau@gmail.com>
2020-05-14 07:52:02 -0300

    Agrega metodo guardar en repositorio de animales

--- src/main/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioDeAnimalesImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioDeAnimalesImpl.java
@@ -4,6 +4,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import ar.edu.unlam.tallerweb1.modelo.AnimalDeGranja;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioDeAnimales;
 
 @Repository
@@ -14,6 +15,11 @@ public class RepositorioDeAnimalesImpl implements RepositorioDeAnimales {
 	@Autowired
 	public RepositorioDeAnimalesImpl(SessionFactory sessionFactory){
 		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public void guardar(AnimalDeGranja animal) {		
+		this.sessionFactory.getCurrentSession().save(animal);
 	}
 
 }
