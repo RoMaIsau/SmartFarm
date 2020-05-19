011320c1567072ecd9d6865a44327217fc52f7a8
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-05-19 18:45:08 -0300

    Se agrego el registro de alimentos

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimentoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimentoImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ar.edu.unlam.tallerweb1.modelo.Alimento;
 import ar.edu.unlam.tallerweb1.modelo.TipoAlimento;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoAlimento;
 
 @Service("servicioalimento")
@@ -14,16 +16,28 @@ import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoAlimento;
 public class ServicioAlimentoImpl implements ServicioAlimento {
 
 	private RepositorioTipoAlimento repositorioTipoAlimento;
+	private RepositorioAlimento repositorioAlimento;
 	
 	@Autowired
-	public ServicioAlimentoImpl(RepositorioTipoAlimento repositorioTipoAlimento) {
+	public ServicioAlimentoImpl(RepositorioTipoAlimento repositorioTipoAlimento, RepositorioAlimento repositorioAlimento) {
 
 		this.repositorioTipoAlimento = repositorioTipoAlimento;
+		this.repositorioAlimento = repositorioAlimento;
 	}
 
 	@Override
 	public List<TipoAlimento> obtenerTiposDeAlimentos() {
 		return (List<TipoAlimento>) repositorioTipoAlimento.obtenerTiposDeAlimentos();
+	}
+
+	@Override
+	public Long registrarAlimento(Alimento alimento) {
+		return repositorioAlimento.registrarAlimento(alimento);
+	}
+
+	@Override
+	public Alimento consultarAlimento(Alimento alimento) {
+		return repositorioAlimento.consultarAlimento(alimento);
 	}
 
 }
