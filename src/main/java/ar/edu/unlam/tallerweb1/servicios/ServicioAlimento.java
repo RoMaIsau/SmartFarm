5e439e65eea24c7c5d1d798069550a3e1213dd30
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-05-19 18:46:58 -0300

    Revert "Se agrego el registro de alimentos"
    This reverts commit 011320c1567072ecd9d6865a44327217fc52f7a8.

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimento.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimento.java
@@ -2,14 +2,9 @@ package ar.edu.unlam.tallerweb1.servicios;
 
 import java.util.List;
 
-import ar.edu.unlam.tallerweb1.modelo.Alimento;
 import ar.edu.unlam.tallerweb1.modelo.TipoAlimento;
 
 public interface ServicioAlimento {
 	
 	List<TipoAlimento> obtenerTiposDeAlimentos();
-
-	Long registrarAlimento(Alimento alimento);
-
-	Alimento consultarAlimento(Alimento Alimento);
 }
--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimentoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimentoImpl.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ar.edu.unlam.tallerweb1.modelo.Alimento;
 import ar.edu.unlam.tallerweb1.modelo.TipoAlimento;
-import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoAlimento;
 
 @Service("servicioalimento")
@@ -16,28 +14,16 @@ import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoAlimento;
 public class ServicioAlimentoImpl implements ServicioAlimento {
 
 	private RepositorioTipoAlimento repositorioTipoAlimento;
-	private RepositorioAlimento repositorioAlimento;
 	
 	@Autowired
-	public ServicioAlimentoImpl(RepositorioTipoAlimento repositorioTipoAlimento, RepositorioAlimento repositorioAlimento) {
+	public ServicioAlimentoImpl(RepositorioTipoAlimento repositorioTipoAlimento) {
 
 		this.repositorioTipoAlimento = repositorioTipoAlimento;
-		this.repositorioAlimento = repositorioAlimento;
 	}
 
 	@Override
 	public List<TipoAlimento> obtenerTiposDeAlimentos() {
 		return (List<TipoAlimento>) repositorioTipoAlimento.obtenerTiposDeAlimentos();
-	}
-
-	@Override
-	public Long registrarAlimento(Alimento alimento) {
-		return repositorioAlimento.registrarAlimento(alimento);
-	}
-
-	@Override
-	public Alimento consultarAlimento(Alimento alimento) {
-		return repositorioAlimento.consultarAlimento(alimento);
 	}
 
 }
