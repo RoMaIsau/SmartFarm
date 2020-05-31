b87bde28ed6e53cfcca8d1196820137fb38f1b27
Rocio Isaurralde <rocio.isau@gmail.com>
2020-05-31 13:07:43 -0300

    Se formatea el codigo

--- src/main/webapp/js/registrarAnimal.js
+++ src/main/webapp/js/registrarAnimal.js
@@ -1,27 +1,26 @@
 (function($) {
-  "use strict"; // Start of use strict
-   $('#tipo').change(function(){	  
-	  
-	  $.getJSON('cargarRazas', {
-		  idTipoAnimal : $(this).val()
-	  }, function(respuesta) {
-		  
-		  var opciones = '';
-		  var cantidadDeRazas = respuesta.length;
-		  
-		  for (var i = 0;  i < cantidadDeRazas; i++) {
-			  
-			  var seleccionada = i == 0 ? 'selected="selected"' : '';
-			  
-			  var raza = respuesta[i];
-			  
-			  opciones += '<option value="' + raza.id + '" ' + seleccionada + '>' + raza.nombre + '</option>';
-		  }	  
-		  
-		  $('#raza').html(opciones);
-		  
-	  });
-  });
-  
+	"use strict"; // Start of use strict
+	$('#tipo').change(
+			function() {
 
+				$.getJSON('cargarRazas', {
+					idTipoAnimal : $(this).val()
+				}, function(respuesta) {
+
+					var opciones = '';
+					var cantidadDeRazas = respuesta.length;
+
+					for (var i = 0; i < cantidadDeRazas; i++) {
+
+						var seleccionada = i == 0 ? 'selected="selected"' : '';
+
+						var raza = respuesta[i];
+
+						opciones += '<option value="' + raza.id + '" ' + seleccionada + '>' + raza.nombre + '</option>';
+					}
+
+					$('#raza').html(opciones);
+
+				});
+			});
 })(jQuery); // End of use strict
