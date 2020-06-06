9b5d5486e9e2f91bc4d26e8a6193780b5fb9321e
Mury10 <murinedducristian@hotmail.com>
2020-06-06 20:31:32 -0300

    Corrección en TipoDeUsuario
    Simplemente permitía duplicados, ahí le puse el "unique = true".

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/TipoDeUsuario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/TipoDeUsuario.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ public class TipoDeUsuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(unique = true)
 	private String nombre;
 	
 	public Long getId() {
