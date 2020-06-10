d4d6c8686244869b839feddff2067d26689436eb
Mury10 <murinedducristian@hotmail.com>
2020-06-10 01:50:26 -0300

    Cambios en el mapa (se agregan errores intencionales)
    Se estableció la posibilidad de que los animales estén posicionados en lugares que no correspondan.
    Fue hecho para cumplir una de las funcionalidades de Trello.

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioUbicacionImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioUbicacionImpl.java
@@ -38,49 +38,100 @@ public class ServicioUbicacionImpl implements ServicioUbicacion {
 
 	public float setearLatitudAleatorea(AnimalDeGranja a) {
 		float n = 0;
-
+		float x = 0;
+		x = (float) ((float) (Math.random() * (100.0 - 1.0)) + 1.0);
+		
 		switch (a.getTipo().getNombre()) {
 
 		case "VACUNO":
-			n = (float) ((float) (Math.random() * (35.277499 - 35.274499)) + 35.274499);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (35.277499 - 35.274499)) + 35.274499);
+			} else {
+				n = (float) ((float) (Math.random() * (35.271174 - 35.268174)) + 35.268174);
+			}
 			break;
+			
 		case "CAPRINO":
-			n = (float) ((float) (Math.random() * (35.282443 - 35.279443)) + 35.279443);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (35.282443 - 35.279443)) + 35.279443);
+			} else {
+				n = (float) ((float) (Math.random() * (35.277380 - 35.274380)) + 35.274380);
+			}
 			break;
+			
 		case "EQUINO":
-			n = (float) ((float) (Math.random() * (35.277380 - 35.274380)) + 35.274380);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (35.277380 - 35.274380)) + 35.274380);
+			} else {
+				n = (float) ((float) (Math.random() * (35.275471 - 35.272471)) + 35.272471);
+			}
 			break;
+			
 		case "OVINO":
-			n = (float) ((float) (Math.random() * (35.275471 - 35.272471)) + 35.272471);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (35.275471 - 35.272471)) + 35.272471);
+			} else {
+				n = (float) ((float) (Math.random() * (35.282443 - 35.279443)) + 35.279443);
+			}
 			break;
+			
 		case "PORCINO":
-			n = (float) ((float) (Math.random() * (35.271174 - 35.268174)) + 35.268174);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (35.271174 - 35.268174)) + 35.268174);
+			} else {
+				n = (float) ((float) (Math.random() * (35.277499 - 35.274499)) + 35.274499);
+			}
 			break;
 		}
-
+		
 		return -n;
 	}
 
 	public float setearLongitudAleatorea(AnimalDeGranja a) {
 		float n = 0;
-		int x = 0;
-
+		float x = 0;
+		x = (float) ((float) (Math.random() * (100.0 - 1.0)) + 1.0);
+		
 		switch (a.getTipo().getNombre()) {
 
 		case "VACUNO":
-			n = (float) ((float) (Math.random() * (59.245634 - 59.242634)) + 59.242634);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (59.245634 - 59.242634)) + 59.242634);
+			} else {
+				n = (float) ((float) (Math.random() * (59.246042 - 59.243042)) + 59.243042);
+			}
 			break;
+			
 		case "CAPRINO":
-			n = (float) ((float) (Math.random() * (59.243749 - 59.240749)) + 59.240749);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (59.243749 - 59.240749)) + 59.240749);
+			} else {
+				n = (float) ((float) (Math.random() * (59.233771 - 59.230771)) + 59.230771);
+			}
 			break;
+			
 		case "EQUINO":
-			n = (float) ((float) (Math.random() * (59.233771 - 59.230771)) + 59.230771);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (59.233771 - 59.230771)) + 59.230771);
+			} else {
+				n = (float) ((float) (Math.random() * (59.257975 - 59.254975)) + 59.254975);
+			}
 			break;
+			
 		case "OVINO":
-			n = (float) ((float) (Math.random() * (59.257975 - 59.254975)) + 59.254975);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (59.257975 - 59.254975)) + 59.254975);
+			} else {
+				n = (float) ((float) (Math.random() * (59.243749 - 59.240749)) + 59.240749);
+			}
 			break;
+			
 		case "PORCINO":
-			n = (float) ((float) (Math.random() * (59.246042 - 59.243042)) + 59.243042);
+			if(x < 99.5) {
+				n = (float) ((float) (Math.random() * (59.246042 - 59.243042)) + 59.243042);
+			} else {
+				n = (float) ((float) (Math.random() * (59.245634 - 59.242634)) + 59.242634);
+			}
 			break;
 		}
 
@@ -88,3 +139,31 @@ public class ServicioUbicacionImpl implements ServicioUbicacion {
 	}
 
 }
+
+/*
+VACUNO: Está mal cuando está en PORCINO
+PORCINO: Está mal cuando está en VACUNO
+CAPRINO: Está mal cuando está en OVINO
+OVINO: Está mal cuando está en EQUINO
+EQUINO: Está mal cuando está en CAPRINO
+
+VACUNO
+(35.271174 - 35.268174)) + 35.268174)
+(59.246042 - 59.243042)) + 59.243042)
+
+CAPRINO
+(35.277380 - 35.274380)) + 35.274380)
+(59.233771 - 59.230771)) + 59.230771)
+
+EQUINO
+(35.275471 - 35.272471)) + 35.272471)
+(59.257975 - 59.254975)) + 59.254975)
+
+OVINO
+(35.282443 - 35.279443)) + 35.279443)
+(59.243749 - 59.240749)) + 59.240749)
+
+PORCINO
+(35.277499 - 35.274499)) + 35.274499)
+(59.245634 - 59.242634)) + 59.242634)
+*/
