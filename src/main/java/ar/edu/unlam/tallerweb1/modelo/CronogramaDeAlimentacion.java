b0dfe78b0468519c9282b90a60b14df3fa479176
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-10 21:11:31 -0300

    Agrega setters y getters

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/CronogramaDeAlimentacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/CronogramaDeAlimentacion.java
@@ -21,4 +21,52 @@ public class CronogramaDeAlimentacion {
 	private PlanAlimentario planAlimentario;
 	@ManyToOne
 	private Alimento alimento;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+
+	public Date getHorario() {
+		return horario;
+	}
+
+	public void setPlanAlimentario(PlanAlimentario planAlimentario) {
+		this.planAlimentario = planAlimentario;
+	}
+
+	public PlanAlimentario getPlanAlimentario() {
+		return planAlimentario;
+	}
+
+	public void setAlimento(Alimento alimento) {
+		this.alimento = alimento;
+	}
+
+	public Alimento getAlimento() {
+		return alimento;
+	}
 }
--- src/main/java/ar/edu/unlam/tallerweb1/modelo/PlanAlimentario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/PlanAlimentario.java
@@ -18,4 +18,35 @@ public class PlanAlimentario {
 	private Boolean estado;
 	private String descripcion;
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
 }
