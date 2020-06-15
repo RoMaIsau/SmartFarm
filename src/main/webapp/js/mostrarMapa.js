67b5b11b4e438d1a8cec1017c38e1fefcde70fa5
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-06-15 12:31:54 -0300

    Agrega estilos a los popups del mapa

--- src/main/webapp/js/mostrarMapa.js
+++ src/main/webapp/js/mostrarMapa.js
@@ -29,11 +29,6 @@ var marker, i;
 
 for (i = 1; i < locations.length; i++) {
 
-	var popup = new mapboxgl.Popup({
-		offset : 25
-	}).setHTML(locations[i][0] + ' - ' + locations[i][3] + ' - '
-			+ locations[i][4]);
-
 	var icono = document.createElement('div');
 	icono.classList.add("icon");
 
@@ -41,20 +36,33 @@ for (i = 1; i < locations.length; i++) {
 
 	case 'VACUNO':
 		icono.id = 'vacuno';
+		imagen = 'vacuno';
 		break;
 	case 'OVINO':
 		icono.id = 'ovino';
+		imagen = 'ovino';
 		break;
 	case 'CAPRINO':
 		icono.id = 'caprino';
+		imagen = 'caprino';
 		break;
 	case 'EQUINO':
 		icono.id = 'equino';
+		imagen = 'equino';
 		break;
 	case 'PORCINO':
 		icono.id = 'porcino';
+		imagen = 'porcino';
 		break;
 	}
+	
+	var popup = new mapboxgl.Popup({
+		offset : 25
+	}).setHTML('<div><div class="row no-gutters align-items-center"><div class="col mr-2">'+
+				'<div class="text-xs font-weight-bold text-success text-uppercase mb-1" style="font-size: 15px;">'+locations[i][0]+'</div>'+
+                 '<div class="h6 mb-0 text-gray-900" style="font-size: 12px;">'+locations[i][3]+' - '+locations[i][4]+'</div></div><div class="col-auto mt-2">'+
+                  '<img class="img-profile" style="height:35px; width: 35px;" src="/SmartFarm/img/'+imagen+'.png"/></div></div> </div>');
+
 
 	new mapboxgl.Marker(icono).setLngLat([ locations[i][1], locations[i][2] ])
 			.setPopup(popup).addTo(map);
