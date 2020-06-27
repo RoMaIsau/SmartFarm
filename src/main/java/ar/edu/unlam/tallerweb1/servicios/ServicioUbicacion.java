9301aa2cca7f7b984e5068872b365201d7220bdc
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-27 16:40:25 -0300

    Se registra la posicion reportada por el gps de un animal

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioUbicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioUbicacion.java
@@ -3,9 +3,12 @@ package ar.edu.unlam.tallerweb1.servicios;
 import java.util.List;
 
 import ar.edu.unlam.tallerweb1.modelo.AnimalUbicacion;
+import ar.edu.unlam.tallerweb1.modelo.Posicion;
 
 public interface ServicioUbicacion {
 
 	List<AnimalUbicacion> obtenerUbicaciones();
+
+	void registrar(Posicion posicion);
 
 }
