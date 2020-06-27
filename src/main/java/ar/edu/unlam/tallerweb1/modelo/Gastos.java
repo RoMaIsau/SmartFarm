b5cd120d33e5ade359ef68a3529f7db57170e637
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-06-27 14:20:03 -0300

    Borra espacios en gastos.java

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Gastos.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Gastos.java
@@ -10,47 +10,54 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Gastos {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@ManyToOne
 	private Usuario usuario;
-	
+
 	private String tipoDeGasto;
 	private Double monto;
 	private String fecha;
-	
-	
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+
 	public String getTipoDeGasto() {
 		return tipoDeGasto;
 	}
+
 	public void setTipoDeGasto(String tipoDeGasto) {
 		this.tipoDeGasto = tipoDeGasto;
 	}
+
 	public Double getMonto() {
 		return monto;
 	}
+
 	public void setMonto(Double monto) {
 		this.monto = monto;
 	}
+
 	public String getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
