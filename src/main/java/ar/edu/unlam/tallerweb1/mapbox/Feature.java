14b8854a0636457e312fff371acdc804da45c269
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-28 19:55:38 -0300

    Agrego constructores por default porque se necesita para que se convierta de json a objeto

--- src/main/java/ar/edu/unlam/tallerweb1/mapbox/Feature.java
+++ src/main/java/ar/edu/unlam/tallerweb1/mapbox/Feature.java
@@ -7,21 +7,35 @@ public class Feature {
 	private Geometry geometry;
 	private Properties properties;
 
-	public Feature(String name, List<Point> points) {
+	public Feature() {}
+
+	public Feature(Long id, String nombre, List<Point> points) {
 		this.type = "Feature";
 		this.geometry = new Geometry(points);
-		this.properties = new Properties("corral", name);
+		this.properties = new Properties("corral", id, nombre);
 	}
 
 	public String getType() {
 		return type;
 	}
 
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public Geometry getGeometry() {
 		return geometry;
 	}
 
+	public void setGeometry(Geometry geometry) {
+		this.geometry = geometry;
+	}
+
 	public Properties getProperties() {
 		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
 	}
 }
