9cc8d71dc17737feb9a1afcf80eee638f14e52e7
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-08 23:03:08 -0300

    Crea test para obtenerTiposDeAlimentos

--- src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimentoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioAlimentoTest.java
@@ -1,25 +1,52 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
 import org.junit.Before;
+import org.junit.Test;
 
+import ar.edu.unlam.tallerweb1.modelo.TipoAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoAlimento;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicioAlimentoTest {
-	
+
 	private ServicioAlimento servicioAlimento;
-	
+
 	private RepositorioTipoAlimento repositorioTipoAlimento;
 	private RepositorioAlimento repositorioAlimento;
-	
+
 	@Before
 	public void inicializar() {
-		
+
 		this.repositorioTipoAlimento = mock(RepositorioTipoAlimento.class);
 		this.repositorioAlimento = mock(RepositorioAlimento.class);
-		
+
 		this.servicioAlimento = new ServicioAlimentoImpl(this.repositorioTipoAlimento, this.repositorioAlimento);
+	}
+
+	@Test
+	public void seObtienenTodosLosTiposDeAlimentoDelRepositorioTipoAlimento() {
+
+		List<TipoAlimento> tiposDeAlimentos = new ArrayList<TipoAlimento>();
+		TipoAlimento tipo1 = new TipoAlimento();
+		tipo1.setId(1L);
+		tipo1.setNombre("Granos");
+
+		tiposDeAlimentos.add(tipo1);
+
+		when(this.repositorioTipoAlimento.obtenerTiposDeAlimentos()).thenReturn(tiposDeAlimentos);
+
+		List<TipoAlimento> tiposDeAlimentoObtenido = this.servicioAlimento.obtenerTiposDeAlimentos();
+
+		verify(this.repositorioTipoAlimento).obtenerTiposDeAlimentos();
+		assertThat(tiposDeAlimentos).hasSize(1);
+		assertThat(tiposDeAlimentos).extracting("nombre").contains("Granos");
+
 	}
 
 }
