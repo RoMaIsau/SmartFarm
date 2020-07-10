d532de08d070c49561d4703a9e7cd56c370b07fd
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-10 19:59:51 -0300

    Crea test de listarNotificacion
    En ServicioNotificacionTest

--- src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioNotificacionTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioNotificacionTest.java
@@ -3,6 +3,8 @@ package ar.edu.unlam.tallerweb1.servicios;
 import org.junit.Before;
 import org.junit.Test;
 
+import ar.edu.unlam.tallerweb1.modelo.Alimento;
+import ar.edu.unlam.tallerweb1.modelo.Notificacion;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioNotificacion;
 
@@ -10,13 +12,17 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicioNotificacionTest {
 	
 	private ServicioNotificacion servicioNotificacion;
 	
 	private RepositorioNotificacion repositorioNotificacion;
 	private RepositorioAlimento repositorioAlimento;
-	private ServicioDeAnimales servicioAnimales;
 	
 	@Before
 	public void inicializar() {
@@ -25,6 +31,34 @@ public class ServicioNotificacionTest {
 		this.repositorioAlimento = mock(RepositorioAlimento.class);
 		
 		this.servicioNotificacion = new ServicioNotificacionImpl(this.repositorioNotificacion, this.repositorioAlimento);
+	}
+	
+//	@Test
+//	public void deberiaCrearNotificacionDeStockMinimo() {
+//		List<Alimento> alimentos = new ArrayList<Alimento>();
+//		Alimento pasto = new Alimento();
+//		pasto.setId(1L);
+//		pasto.setCantidad(10.0);
+//		pasto.setStockMinimo(15.0);
+//		pasto.setNombre("Pasto");
+//		alimentos.add(pasto);
+//		
+//		when(this.repositorioAlimento.listarAlimentos()).thenReturn(alimentos);
+//		
+//		this.servicioNotificacion.crearNotificacionStock();
+//		
+//		verify(this.repositorioAlimento).listarAlimentos();
+//		
+//	}
+	
+	@Test
+	public void deberiaListarLasNotificacionesPorIdDeUsuario() {
+		Long idUsuario = 1L;
+		
+		List<Notificacion> notificaciones = this.servicioNotificacion.listarNotificaciones(idUsuario);
+		
+		verify(this.repositorioNotificacion).listarNotificaciones(eq(idUsuario));
+		assertNotNull(notificaciones);
 	}
 	
 	
