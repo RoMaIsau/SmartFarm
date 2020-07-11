a469d9ff03bedd3148937f49fbbb0ff704728b76
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-11 13:16:57 -0300

    Crea test cuandoElRolEsAdminRedirigeALaVistaIndexAdmin
    En ControladorUsuarioTest

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuarioTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuarioTest.java
@@ -1,19 +1,53 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import static org.mockito.Mockito.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioUsuario;
 
 public class ControladorUsuarioTest {
-	
+
 	private ControladorUsuario controladorUsuario;
 	private ServicioUsuario servicioUsuario;
-	
+
 	@Before
 	public void inicializar() {
 		this.servicioUsuario = mock(ServicioUsuario.class);
-		
+
 		this.controladorUsuario = new ControladorUsuario(this.servicioUsuario);
+	}
+
+	@Test
+	public void cuandoElRolEsAdminRedirigeALaVistaIndexAdmin() {
+		HttpServletRequest request = configurarRolLogueado("Admin");
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+
+		when(servicioUsuario.consultarUsuario(usuario)).thenReturn(usuario);
+
+		ModelAndView modelAndView = this.controladorUsuario.validarLogin(usuario, request);
+
+		assertThat(modelAndView.getViewName()).isEqualTo("redirect:/indexAdmin");
+	}
+
+	private HttpServletRequest configurarRolLogueado(String rol) {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpSession session = mock(HttpSession.class);
+
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("ROL")).thenReturn(rol);
+
+		return request;
+
 	}
 }
