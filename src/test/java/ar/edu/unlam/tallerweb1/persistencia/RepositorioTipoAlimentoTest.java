370ba232f65e879e242f03aaca4b4b987f064ded
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-14 17:50:21 -0300

    Crea test para obtenerTiposDeAlimentos

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioTipoAlimentoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioTipoAlimentoTest.java
@@ -6,13 +6,16 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
 import org.junit.Before;
 import org.junit.Test;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.TipoAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoAlimentoImpl;
 
+@Transactional
 public class RepositorioTipoAlimentoTest extends SpringTest {
 	
 	private RepositorioTipoAlimento repositorioTipoAlimento;
@@ -22,5 +25,20 @@ public class RepositorioTipoAlimentoTest extends SpringTest {
 		this.repositorioTipoAlimento = new RepositorioTipoAlimentoImpl(this.sessionFactory);
 	}
 	
-	
+	@Test
+	public void deberiaObtenerTodosLosTiposDeAlimentos() {
+		TipoAlimento cereal = new TipoAlimento();
+		cereal.setNombre("Cereal");
+		
+		TipoAlimento granos = new TipoAlimento();
+		granos.setNombre("Granos");
+		
+		Session session = this.sessionFactory.getCurrentSession();
+		session.save(cereal);
+		session.save(granos);
+		
+		List<TipoAlimento> tiposObtenidos = this.repositorioTipoAlimento.obtenerTiposDeAlimentos();
+		
+		assertThat(tiposObtenidos).hasSize(2);
+	}
 }
