1434d213afb60c6d59988301cfac00f7dc4ff280
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-14 16:20:19 -0300

    Agrega test de registrarAlimento
    En RepositorioAlimentoTest

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioAlimentoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioAlimentoTest.java
@@ -1,20 +1,51 @@
 package ar.edu.unlam.tallerweb1.persistencia;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
 import javax.transaction.Transactional;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Alimento;
+import ar.edu.unlam.tallerweb1.modelo.TipoAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimentoImpl;
 
 @Transactional
-public class RepositorioAlimentoTest extends SpringTest{
-	
+public class RepositorioAlimentoTest extends SpringTest {
+
 	private RepositorioAlimento repositorioAlimento;
-	
+
 	@Before
 	public void inicializar() {
 		this.repositorioAlimento = new RepositorioAlimentoImpl(this.sessionFactory);
 	}
-}	
+	
+	@Test
+	public void deberiaGuardarUnAlimento() {
+		Alimento alimento = this.crearAlimento("Arroz", 220.0, 50.0, "Granos");
+		
+		this.repositorioAlimento.registrarAlimento(alimento);
+		
+		assertNotNull(alimento.getId());
+		assertNotNull(alimento.getTipo().getId());
+	}
+	
+	private Alimento crearAlimento(String nombre, Double cantidad, Double stockMinimo, String tipo) {
+		TipoAlimento tipoAlimento = new TipoAlimento();
+		tipoAlimento.setNombre(tipo);
+		
+		this.sessionFactory.getCurrentSession().save(tipoAlimento);
+		
+		Alimento alimento = new Alimento();
+		alimento.setNombre(nombre);
+		alimento.setCantidad(cantidad);
+		alimento.setStockMinimo(stockMinimo);
+		alimento.setTipo(tipoAlimento);
+		
+		return alimento;
+	}
+}
