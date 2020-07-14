ddf697d16a5345056f0e67b317b212d39a5cf620
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-14 17:39:39 -0300

    Crea test de listarAlimentosConsumidosPorAnimal

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioAlimentoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioAlimentoTest.java
@@ -7,12 +7,20 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
 import org.junit.Before;
 import org.junit.Test;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
 import ar.edu.unlam.tallerweb1.modelo.Alimento;
+import ar.edu.unlam.tallerweb1.modelo.AnimalDeGranja;
+import ar.edu.unlam.tallerweb1.modelo.CronogramaDeAlimentacion;
+import ar.edu.unlam.tallerweb1.modelo.CronogramaDeAlimentacion.EstadoCronograma;
+import ar.edu.unlam.tallerweb1.modelo.Genero;
+import ar.edu.unlam.tallerweb1.modelo.PlanAlimentario;
+import ar.edu.unlam.tallerweb1.modelo.Raza;
 import ar.edu.unlam.tallerweb1.modelo.TipoAlimento;
+import ar.edu.unlam.tallerweb1.modelo.TipoAnimal;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimento;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimentoImpl;
 
@@ -20,7 +28,7 @@ import ar.edu.unlam.tallerweb1.repositorios.RepositorioAlimentoImpl;
 public class RepositorioAlimentoTest extends SpringTest {
 
 	private RepositorioAlimento repositorioAlimento;
-
+	
 	@Before
 	public void inicializar() {
 		this.repositorioAlimento = new RepositorioAlimentoImpl(this.sessionFactory);
@@ -89,6 +97,20 @@ public class RepositorioAlimentoTest extends SpringTest {
 		assertThat(alimentoObtenido.getCantidad()).isEqualTo(300.0);
 	}
 	
+	@Test
+	public void deberiaListarAlimentosConsumidosPorUnAnimal() {
+		Alimento arroz = this.crearAlimento("Arroz", 220.0, 50.0, "Granos");
+		this.repositorioAlimento.registrarAlimento(arroz);
+		
+		CronogramaDeAlimentacion cronograma = this.crearCronograma(arroz);
+		
+		Long idAnimal = cronograma.getPlanAlimentario().getAnimal().getId();
+		
+		List<Alimento> alimentoConsumidos = this.repositorioAlimento.listarAlimentosConsumidosPorAnimal(idAnimal);
+		
+		assertThat(alimentoConsumidos).hasSize(1);
+	}
+	
 	private Alimento crearAlimento(String nombre, Double cantidad, Double stockMinimo, String tipo) {
 		TipoAlimento tipoAlimento = new TipoAlimento();
 		tipoAlimento.setNombre(tipo);
@@ -102,5 +124,46 @@ public class RepositorioAlimentoTest extends SpringTest {
 		alimento.setTipo(tipoAlimento);
 
 		return alimento;
+	}
+	
+	private CronogramaDeAlimentacion crearCronograma(Alimento alimento) {
+		TipoAnimal tipo = new TipoAnimal();
+		tipo.setNombre("VACUNO");
+		
+		Raza raza = new Raza();
+		raza.setTipo(tipo);
+		raza.setNombre("Vaquita");
+		
+		Genero genero = new Genero();
+		genero.setNombre("MACHO");
+		
+		Session session = this.sessionFactory.getCurrentSession();
+		session.save(tipo);
+		session.save(raza);
+		session.save(genero);
+		
+		AnimalDeGranja animal = new AnimalDeGranja();
+		animal.setTipo(tipo);
+		animal.setRaza(raza);
+		animal.setGenero(genero);
+		animal.setIdentificadorGps("GPS0001");
+		
+		session.save(animal);
+		
+		PlanAlimentario plan = new PlanAlimentario();
+		plan.setAnimal(animal);
+		
+		session.save(plan);
+		
+		CronogramaDeAlimentacion cronograma = new CronogramaDeAlimentacion();
+		cronograma.setId(1L);
+		cronograma.setPlanAlimentario(plan);
+		cronograma.setAlimento(alimento);
+		cronograma.setCantidad(10);
+		cronograma.setEstado(EstadoCronograma.COMPLETO);
+		
+		session.save(cronograma);
+		
+		return cronograma;
 	}
 }
