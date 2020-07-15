144551c9995d11c0864f13b07cc4b104299d6898
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-14 21:55:15 -0300

    Agrea test de obtenerTiposDeGastos

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioTipoDeGastoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/RepositorioTipoDeGastoTest.java
@@ -1,10 +1,17 @@
 package ar.edu.unlam.tallerweb1.persistencia;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
 import org.junit.Before;
+import org.junit.Test;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.TipoDeGasto;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoDeGasto;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioTipoDeGastoImpl;
 
@@ -16,5 +23,23 @@ public class RepositorioTipoDeGastoTest extends SpringTest{
 	@Before
 	public void inicializar() {
 		this.repositorioTipoDeGasto = new RepositorioTipoDeGastoImpl(this.sessionFactory);
+	}
+	
+	@Test
+	public void deberiaObtenerTodosLosTiposDeGastos() {
+		TipoDeGasto alimenticio = new TipoDeGasto();
+		alimenticio.setNombre("Alimenticio");
+		
+		TipoDeGasto medico = new TipoDeGasto();
+		medico.setNombre("Medico");
+		
+		Session session = this.sessionFactory.getCurrentSession();
+		session.save(alimenticio);
+		session.save(medico);
+		
+		List<TipoDeGasto> tipos = this.repositorioTipoDeGasto.obtenerTiposDeGastos();
+		
+		assertThat(tipos).hasSize(2);
+		assertThat(tipos).extracting("nombre").contains("Medico");
 	}
 }
