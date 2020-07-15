a928fa55071439a5d8e6c330b32f7f9ff539f211
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-07-15 13:35:00 -0300

    Crea test para irAMapa

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorMapaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorMapaTest.java
@@ -1,9 +1,23 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
 
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import ar.edu.unlam.tallerweb1.modelo.AnimalUbicacion;
+import ar.edu.unlam.tallerweb1.modelo.Notificacion;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioAlimento;
 import ar.edu.unlam.tallerweb1.servicios.ServicioAnimalUbicacion;
 import ar.edu.unlam.tallerweb1.servicios.ServicioDeAnimales;
@@ -34,5 +48,60 @@ public class ControladorMapaTest {
 
 		this.controladorMapa = new ControladorMapa(this.servicioUbicacion, this.servicioNotificacion,
 				this.servicioDeAnimales, this.servicioAnimalUbicacion, this.servicioAlimento, this.servicioVacunas);
+	}
+	
+	@Test
+	public void siElRolNoEsEmpleadoNiAdminNiVeterinarioRedirigeALaVistaLogin() {
+		HttpServletRequest request = configurarRolLogueado("");
+	
+		ModelMap modelo = new ModelMap();
+		
+		ModelAndView modelAndView = this.controladorMapa.irAMapa(request, modelo);
+		
+		assertThat(modelAndView.getViewName()).isEqualTo("redirect:/login");
+	}
+	
+	@Test
+	public void dirigeAVistaMapaConUbicacionesYNotificaciones() {
+		HttpServletRequest request = configurarRolLogueado("Empleado");
+		
+		AnimalUbicacion animalUbicacion = new AnimalUbicacion();
+		animalUbicacion.setId(1L);
+		
+		Notificacion notificacion = new Notificacion();
+		notificacion.setId(1L);
+		
+		List<AnimalUbicacion> listaUbicaciones = new ArrayList<AnimalUbicacion>();
+		listaUbicaciones.add(animalUbicacion);
+		
+		List<Notificacion> listaNotificaciones= new ArrayList<Notificacion>();
+		listaNotificaciones.add(notificacion);
+		
+		when(this.servicioUbicacion.obtenerUbicaciones()).thenReturn(listaUbicaciones);
+		when(this.servicioNotificacion.listarNotificaciones(1L)).thenReturn(listaNotificaciones);
+		
+		ModelAndView modelAndView = this.controladorMapa.irAMapa(request, new ModelMap());
+		
+		assertThat(modelAndView.getViewName()).isEqualTo("mapa");
+		assertThat(modelAndView.getModelMap()).containsKey("lista");
+		assertThat(modelAndView.getModelMap()).containsKey("notificaciones");
+		
+		List<AnimalUbicacion> aUbiObtenidos = (List<AnimalUbicacion>) modelAndView.getModelMap().get("lista");
+		List<Notificacion> notificacionesObtenidos = (List<Notificacion>) modelAndView.getModelMap().get("notificaciones");
+		
+		assertThat(aUbiObtenidos).isNotEmpty();
+		assertThat(notificacionesObtenidos).isNotEmpty();
+	}
+	
+	private HttpServletRequest configurarRolLogueado(String rol) {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpSession session = mock(HttpSession.class);
+
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("ROL")).thenReturn(rol);
+		when(session.getAttribute("ID")).thenReturn(1L);
+
+		return request;
+
 	}
 }
